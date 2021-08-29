8300c7367b64cfd21af23eee63e66265db18eda7
maxim <maxxmall88@gmail.com>
2021-08-29 22:28:36 +0300

    Merge branch 'module1-task2' of github.com:StormWindRunner/1534899-typoteka-3 into module1-task2

---
+++ src/service/cli/version.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const packageJSON = require('../../../package.json');
+module.exports = {
+    name: '--version',
+    run () {
+        console.log('Version is >>>', packageJSON.version)
+    },
+};
--- src/service/service.js
+++ src/service/service.js
@@ -1,4 +1,14 @@
-'use strict';
+"use strict";
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+const {USER_ARGV_INDEX, DEFAULT_COMMAND, EXIT_CODES, CATEGORIES} = require(`./constants`);
 
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(EXIT_CODES.codeFailure);
+}
+
+Cli[userCommand].run(userArguments.slice(1));
