f21b69bff3b7812ba6d6b21f1fbbf1351bda0449
MaksimMalakhovSimbirsoft <maksim.malakhov@simbirsoft.com>
2022-07-24 23:48:09 +0300

    lastChanges

--- src/express/express.js
+++ src/express/express.js
@@ -8,17 +8,42 @@ const mainRouter = require(`./routes/main-routes`);
 const myRouter = require(`./routes/my-routes`);
 const articlesRouter = require(`./routes/articles-routes`);
 
+const session = require(`express-session`);
+const sequelize = require(`../service/lib/sequalize`);
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
+
 const app = express();
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
 const UPLOAD_DIR = `upload`;
 
+const {SESSION_SECRET} = process.env;
+if (!SESSION_SECRET) {
+  throw new Error(`SESSION_SECRET environment variable is not defined`);
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
+
+const mySessionStore = new SequelizeStore({
+  db: sequelize,
+  expiration: 180000,
+  checkExpirationInterval: 60000
+});
+
+sequelize.sync({force: false});
+
+app.use(session({
+  secret: SESSION_SECRET,
+  store: mySessionStore,
+  resave: false,
+  proxy: true,
+  saveUninitialized: false,
+}));
 
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,10 +3,13 @@
 const {Router} = require(`express`);
 const api = require(`../api`).getAPI();
 const upload = require(`../middleware/picture-upload`);
+const csrf = require(`csurf`);
 const mainRouter = new Router();
 const {prepareErrors} = require(`../../utils`);
 
+const csrfProtection = csrf();
 const ARTICLES_PER_PAGE = 8;
+
 
 // Все категории GET
 mainRouter.get(`/categories`, async (req, res) => {
@@ -80,24 +83,54 @@ mainRouter.post(`/register`, upload.single(`avatar`), async (req, res) => {
 //*************************/ Логин пользователя /**********************************//
 
 // Роуты для регистрации и логина
-mainRouter.get(`/login`, async (req, res) => {
-  res.render(`authorization/login`);
+mainRouter.get(`/login`, csrfProtection, async (req, res) => {
+  res.render(`authorization/login`, {csrfToken: req.csrfToken()});
 });
 
+// mainRouter.get(`/login`, csrfProtection, (req, res) => res.render(`login`, {csrfToken: req.csrfToken()}));
+
 // Логин пользователя
-mainRouter.post(`/login`, async (req, res) => {
-  const {body} = req;
+// mainRouter.post(`/login`, async (req, res) => {
+//   const {body} = req;
+  // const userData = {
+  //   email: body[`email`],
+  //   password: body[`password`],
+  // };
+//   await api.auth(userData);
+//   res.redirect(`/`);
+// });
+
+mainRouter.post(`/login`, csrfProtection, async (req, res) => {
   const userData = {
-    email: body[`email`],
-    password: body[`password`],
+    email: req.body[`email`],
+    password: req.body[`password`],
   };
-  await api.auth(userData);
+  console.log('userData', userData);
+
+  try {
+    const user = await api.auth(userData);
+    console.log('user>>>', user);
+    req.session.user = user;
+    req.session.save(() => {
+      res.redirect(`/`);
+    });
+  } catch (errors) {
+    const validationMessages = prepareErrors(errors);
+    const {user} = req.session;
+    res.render(`login`, {user, validationMessages});
+  }
+});
+
+// Logout
+mainRouter.get(`/logout`, (req, res) => {
+  delete req.session.user;
   res.redirect(`/`);
 });
 
 // Главная страница, получение статей с комментариями и категориями
 mainRouter.get(`/`, async (req, res) => {
   let {page = 1} = req.query;
+  const {user} = req.session;
   page = +page;
 
   const limit = ARTICLES_PER_PAGE;
@@ -117,7 +150,7 @@ mainRouter.get(`/`, async (req, res) => {
 
   const totalPages = Math.ceil(count / ARTICLES_PER_PAGE);
 
-  res.render(`main`, {articles, categories, popularArticles, lastComments, page, totalPages});
+  res.render(`main`, {articles, page, totalPages, categories, user, popularArticles, lastComments});
 });
 
 // Страница поиска
