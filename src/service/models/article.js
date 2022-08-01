40ff19753299947bde5d881df91d1a78d2d3a922
MaksimMalakhovSimbirsoft <maksim.malakhov@simbirsoft.com>
2022-08-01 10:30:29 +0300

    fixes-final

--- src/service/models/article.js
+++ src/service/models/article.js
@@ -1,23 +1,24 @@
 "use strict";
 
 const {DataTypes, Model} = require(`sequelize`);
+const {STRING_LENGTH} = require(`../constants`);
 
 class Article extends Model {}
 
 const defineArticle = (sequelize) => Article.init({
   title: {
     // eslint-disable-next-line new-cap
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING(STRING_LENGTH),
     allowNull: false,
   },
   fullText: {
     // eslint-disable-next-line new-cap
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING(STRING_LENGTH),
     allowNull: false,
   },
   announce: {
     // eslint-disable-next-line new-cap
-    type: DataTypes. STRING(255),
+    type: DataTypes. STRING(STRING_LENGTH),
     allowNull: false,
   },
   image: {
--- src/service/models/user.js
+++ src/service/models/user.js
@@ -1,30 +1,31 @@
 "use strict";
 
 const {DataTypes, Model} = require(`sequelize`);
+const {STRING_LENGTH} = require(`../constants`);
 
 class User extends Model {}
 
 const defineUser = (sequelize) => User.init({
   firstName: {
     // eslint-disable-next-line new-cap
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING(STRING_LENGTH),
     allowNull: false,
   },
   lastName: {
     // eslint-disable-next-line new-cap
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING(STRING_LENGTH),
     allowNull: false,
   },
 
   email: {
     // eslint-disable-next-line new-cap
-    type: DataTypes. STRING(255),
+    type: DataTypes. STRING(STRING_LENGTH),
     allowNull: false,
     unique: true,
   },
   passwordHash: {
     // eslint-disable-next-line new-cap
-    type: DataTypes. STRING(255),
+    type: DataTypes. STRING(STRING_LENGTH),
     allowNull: false,
   },
   avatar: {
